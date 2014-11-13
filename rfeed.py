c2244e82d600c51faf3ee765bf5c5ec4cb1c6b07
svpino <svpino@gmail.com>
2014-11-13 09:02:29 -0500

    ...

--- rfeed.py
+++ rfeed.py
@@ -1,4 +1,10 @@
+__name__ = "rfeed"
+__version__ = (1, 0, 0)
+__author__ = "Santiago L. Valdarrama - https://blog.svpino.com"
+_generator = __name__ + " v" + ".".join(map(str, __version__))
+
 import sys
+import datetime
 from StringIO import StringIO
 from xml.sax import saxutils
 
@@ -15,6 +21,26 @@ class Serializable:
 	def _publish(self):
 		# This is the method that subclasses should override
 		pass
+
+	def date(self, date):
+		""" Converts a datetime into an RFC 822 formatted date.
+		This method assumes that the input date is in GMT.
+		Returns None if None is provided as an argument.
+		"""
+
+		# Alright, I admit it: this method looks hideous. The thing is that RFC 822 requires a specific format for dates, and strftime is 
+		# locale dependent, so I can't use it to create the final date unless I force change the system locale.
+		#
+		# I looked into that (locale.setlocale, then restore), but I got the feeling that I was doing things that I was going to regret later.
+		# Maybe it's just me, but it doesn't feel right to force change the locale just to create a simple date.
+		#
+		# So, not having a better solution, I went ahead and used the original method from the PyRSS2Gen library.
+
+		if date is None:
+			return None
+		
+		return "%s, %02d %s %04d %02d:%02d:%02d GMT" % (["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"][date.weekday()], date.day,
+            ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"][date.month-1], date.year, date.hour, date.minute, date.second)
 
 class iTunes(Serializable):
 	def __init__(self, author, block, category, image, explicit, complete, new_feed_url, owner, subtitle, summary):
@@ -45,7 +71,20 @@ class Item(Serializable):
 		self.handler.endElement("item")
 
 class Feed(Serializable):
-	def __init__(self, title, link, description, language = None, copyright = None, managingEditor = None, webMaster = None, items = None):
+	def __init__(self, 
+		title, 						# The name of the channel.
+		link, 						# The URL to the HTML website corresponding to the channel.
+		description, 				# Phrase or sentence describing the channel.
+		language = None, 			# The language the channel is written in.
+		copyright = None, 			# Copyright notice for content in the channel.
+		managingEditor = None, 		# Email address for person responsible for editorial content.
+		webMaster = None, 			# Email address for person responsible for technical issues relating to channel.
+		pubDate = None,				# The publication date for the content in the channel. This should be a datetime in GMT format.
+		lastBuildDate = None,		# The last time the content of the channel changed. This should be a datetime in GMT format.
+		generator = None,			# A string indicating the program used to generate the channel.
+
+		items = None):
+
 		Serializable.__init__(self)
 
 		if title is None: raise ElementRequiredError("title")
@@ -59,6 +98,10 @@ class Feed(Serializable):
 		self.copyright = copyright
 		self.managingEditor = managingEditor
 		self.webMaster = webMaster
+		self.pubDate = pubDate
+		self.lastBuildDate = lastBuildDate
+
+		self.generator = _generator if generator is None else generator
 
 		self.items = [] if items is None else items
 
@@ -78,6 +121,9 @@ class Feed(Serializable):
 		self._write_element("copyright", self.copyright)
 		self._write_element("managingEditor", self.managingEditor)
 		self._write_element("webMaster", self.webMaster)
+		self._write_element("pubDate", self.date(self.pubDate))
+		self._write_element("lastBuildDate", self.date(self.lastBuildDate))
+		self._write_element("generator", self.generator)
 
 		for item in self.items:
 			item.handler = self.handler
--- tests.py
+++ tests.py
@@ -1,11 +1,24 @@
 import unittest
+import locale
+from time import gmtime, strftime
+from datetime import datetime
 from rfeed import *
+
+class SerializableTestCase(unittest.TestCase):
+
+	def test_date(self):
+		self.assertEquals('Thu, 13 Nov 2014 08:00:00 GMT', Serializable().date(datetime.datetime(2014, 11, 13, 8, 0, 0)))
+		self.assertEquals('Mon, 01 Dec 2014 10:22:15 GMT', Serializable().date(datetime.datetime(2014, 12, 1, 10, 22, 15)))
+
+	def test_date_returns_none_if_date_is_none(self):
+		self.assertIsNone(Serializable().date(None))
 
 class FeedTestCase(unittest.TestCase):
 
 	def test_required_elements(self):
+
 		self.assertTrue(self._element('title', 'This is a sample title') in Feed('This is a sample title', '', '').rss())
-		self.assertTrue(self._element('link', 'https://rfeed.svpino.com') in Feed('', 'https://rfeed.svpino.com', '').rss())
+		self.assertTrue(self._element('link', 'https://www.google.com') in Feed('', 'https://www.google.com', '').rss())
 		self.assertTrue(self._element('description', 'This is a sample description') in Feed('', '', 'This is a sample description').rss())
 
 	def test_required_elements_validation(self):
@@ -26,13 +39,15 @@ class FeedTestCase(unittest.TestCase):
 		self.assertTrue(self._element('copyright', 'Copyright 2014') in Feed('', '', '', copyright = 'Copyright 2014').rss())
 		self.assertTrue(self._element('managingEditor', 'John Doe') in Feed('', '', '', managingEditor = 'John Doe').rss())
 		self.assertTrue(self._element('webMaster', 'john@doe.com') in Feed('', '', '', webMaster = 'john@doe.com').rss())
+		self.assertTrue(self._element('pubDate', 'Thu, 13 Nov 2014 08:00:00 GMT') in Feed('', '', '', pubDate = datetime.datetime(2014, 11, 13, 8, 0, 0)).rss())
+		self.assertTrue(self._element('lastBuildDate', 'Mon, 01 Dec 2014 10:22:15 GMT') in Feed('', '', '', lastBuildDate = datetime.datetime(2014, 12, 1, 10, 22, 15)).rss())
+		self.assertTrue(self._element('generator', 'Name goes here') in Feed('', '', '', generator = 'Name goes here').rss())
 
-	def test_optional_elements_are_abscent(self):
-		rss = Feed('title', 'link', 'description').rss()
-		self.assertFalse('language' in rss)
-		self.assertFalse('copyright' in rss)
-		self.assertFalse('managingEditor' in rss)
-		self.assertFalse('webMaster' in rss)
+	def test_if_generator_not_specified_use_default_value(self):
+		# I'm partially checking for the element because the value includes the version number and
+		# changing it will break the test. By just doing a partial match, I make sure the test keeps
+		# working in future versions as well.
+		self.assertTrue('<generator>rfeed v' in Feed('', '', '').rss())		
 
 	def _element(self, element, value):
 		return '<' + element + '>' + value + '</' + element + '>'
