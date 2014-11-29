5825a27aa74db39dd74f59d467e82bf9faedbe1f
svpino <svpino@gmail.com>
2014-11-29 12:45:11 -0500

    Added setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name = "rfeed",
+      version = "1.0.0",
+      description = "Python RSS 2.0 Generator",
+      author = "Santiago L. Valdarrama",
+      author_email = "svpino@gmail.com",
+      url = "https://github.com/svpino/rfeed/blob/master/README.md",
+      license = "MIT",
+      py_modules = ["rfeed"])
